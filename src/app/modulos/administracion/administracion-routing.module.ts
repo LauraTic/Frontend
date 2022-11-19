8f517a1561e9effc60402c6733479e85c6f97f20
LauraTic <laurac.cardona@udea.edu.co>
2022-11-18 20:37:25 -0500

    Mascotas2

--- src/app/modulos/administracion/administracion-routing.module.ts
+++ src/app/modulos/administracion/administracion-routing.module.ts
@@ -106,7 +106,7 @@ const routes: Routes = [
   {
     path:"crear-prospecto",
     component:CrearProspectoComponent
-  }
+  },
  
 
 
--- src/app/servicios/mascotas.service.ts
+++ src/app/servicios/mascotas.service.ts
@@ -1,9 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MascotasService {
+  url='http://127.0.0.1:3000';
+  token:string='';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 }
