a3bfa183e37b243c8248ab048617a2f1b6b78f1a
Ju4nch026 <ju4nch0s18@gmail.com>
2022-11-22 21:47:07 -0500

    tbl_2

---
+++ src/app/modulos/administracion/prueba-tbl/prueba-tbl.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prueba-tbl',
+  templateUrl: './prueba-tbl.component.html',
+  styleUrls: ['./prueba-tbl.component.css']
+})
+export class PruebaTblComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
